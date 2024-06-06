c2c4b60380e0cd2a87e30e38f5486c23701959e0
ArthurGDamasceno <gomesdamascenoarthur@gmail.com>
2024-06-05 23:09:41 -0300

    update DeclarandoVariaveis

---
+++ java-anatomia-classes/declarando-variaveis/src/DeclarandoVariaveis.java
@@ -0,0 +1,10 @@
+public class DeclarandoVariaveis {
+    
+    //Estrutura 
+    //Tipo nomeBemDefinido = Atribuição (Opcional) 
+
+    //Exemplo
+    String meuNome = "Arthur";
+    double altura = 1.63;
+    int numero = 123;
+}
